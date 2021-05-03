43c8afb99589db4d2b2a537e66a768e421bb1dee
robinsrk <robinsrk3@gmail.com>
2021-05-03 19:26:17 +0600

    added animation

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Bounce } from "react-reveal";
 import {
   MDBBtn,
   MDBCard,
@@ -13,32 +14,34 @@ import "./Project.css";
 const Project = ({ projectDetails }) => {
   console.log("projectDetails = ", projectDetails);
   return (
-    <MDBCol>
-      <MDBCard className="cards" style={{ width: "22rem" }}>
-        <MDBCardImage
-          className="img-fluid"
-          style={{ borderRadius: "10px" }}
-          src={projectDetails.photo}
-          waves
-        />
-        <MDBCardBody>
-          <MDBCardTitle>{projectDetails.name}</MDBCardTitle>
-          <MDBCardText>
-            <ul>
-              {projectDetails.features.map((ft) => (
-                <li>{ft}</li>
-              ))}
-            </ul>
-          </MDBCardText>
-          <MDBBtn href="#" className="button">
-            Website
-          </MDBBtn>
-          <MDBBtn href="#" className="button">
-            Source Code
-          </MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
+    <Bounce>
+      <MDBCol>
+        <MDBCard className="cards" style={{ width: "22rem" }}>
+          <MDBCardImage
+            className="img-fluid"
+            style={{ borderRadius: "10px" }}
+            src={projectDetails.photo}
+            waves
+          />
+          <MDBCardBody>
+            <MDBCardTitle>{projectDetails.name}</MDBCardTitle>
+            <MDBCardText>
+              <ul>
+                {projectDetails.features.map((ft) => (
+                  <li>{ft}</li>
+                ))}
+              </ul>
+            </MDBCardText>
+            <MDBBtn href="#" className="button">
+              Website
+            </MDBBtn>
+            <MDBBtn href="#" className="button">
+              Source Code
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </Bounce>
   );
 };
 
