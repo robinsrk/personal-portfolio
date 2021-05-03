2e441df4268390d2b7359d214015c18e8695b47f
robinsrk <robinsrk3@gmail.com>
2021-05-03 11:23:14 +0600

    added font border

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -20,32 +20,37 @@ class NavbarPage extends Component {
 
   render() {
     return (
-      <MDBNavbar expand="md" dark className="navbar">
+      <MDBNavbar
+        expand="md"
+        dark
+        className="navbar"
+        style={{ fontWeight: "bold", color: "#fa6454" }}
+      >
         <MDBNavbarToggler onClick={this.toggleCollapse} />
         <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
           <MDBNavbarNav right>
             <MDBNavItem active>
-              <Link className="m-5 text-white" smooth to="#home">
+              <Link className="m-5 text-white texts" smooth to="#home">
                 Home
               </Link>
             </MDBNavItem>
             <MDBNavItem>
-              <Link smooth className="m-5 text-white" to="#projects">
+              <Link smooth className="m-5 text-white texts" to="#projects">
                 Projects
               </Link>
             </MDBNavItem>
             <MDBNavItem>
-              <Link smooth className="m-5 text-white" to="#blogs">
+              <Link smooth className="m-5 text-white texts" to="#blogs">
                 Blogs
               </Link>
             </MDBNavItem>
             <MDBNavItem>
-              <Link smooth className="m-5 text-white" to="#about">
+              <Link smooth className="m-5 text-white texts" to="#about">
                 About
               </Link>
             </MDBNavItem>
             <MDBNavItem>
-              <Link smooth className="m-5 text-white" to="#contactus">
+              <Link smooth className="m-5 text-white texts" to="#contactus">
                 Contact Me
               </Link>
             </MDBNavItem>
