c2b7577883ab63509bb82352b2d4f6b8546cb6e8
robinsrk <robinsrk3@gmail.com>
2021-05-08 10:35:53 +0600

    added padding

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,7 +4,7 @@ import React from "react";
 const Skills = ({ skill }) => {
   console.log(skill);
   return (
-    <MDBCol lg="2" size="6">
+    <MDBCol lg="2" size="6" className="p-3">
       <img alt="" style={{ width: "100px" }} src={skill.photo} />
     </MDBCol>
   );
