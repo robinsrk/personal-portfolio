c6da5a8771f93ed53ae71e7fe8f09377e15819f3
robinsrk <robinsrk3@gmail.com>
2021-05-08 11:53:53 +0600

    updated title

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,8 @@ import "./Blogs.css";
 const Blogs = () => {
   return (
     <div id="blogs">
-      <h1 className="title">Blogs coming soon</h1>
+      <h1 className="title">Blogs</h1>
+      <h4 className="title">Blogs coming soon</h4>
     </div>
   );
 };
